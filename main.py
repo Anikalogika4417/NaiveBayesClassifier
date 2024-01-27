fb1f0b01ef94bc036f645223b54e55fb7d62cc15
Anikalogika4417 <anikalogika4417@gmail.com>
2024-01-27 19:18:43 +0200

    Start

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
 
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
+categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+training_data = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=True)
 
-credit_data = pd.read_csv('credit_data.csv')
-
-features = credit_data[['income', 'age', 'loan']]
-target = credit_data.default
-
-feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.3)
-
-model = GaussianNB()
-fettedModel = model.fit(feature_train, target_train)
-prediction = fettedModel.predict(feature_test)
-
-print(confusion_matrix(target_test, prediction))
-print(accuracy_score(target_test, prediction))
+print("Target is: ", training_data.target_names[training_data.target[0]])
+#print("\n".join(training_data.data[0].split("\n")[:10]))
